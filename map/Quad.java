dd9d8da2a3fafc83e8aad8e1855183a561455c80
Extremelyd1 <Extremelyd1@outlook.com>
2015-04-01 19:26:15 +0200

    Worked on things

--- map/Quad.java
+++ map/Quad.java
@@ -7,13 +7,15 @@ import java.awt.Graphics2D;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import com.sun.javafx.geom.Line2D;
+
 public class Quad {
 
 	private Vertex p1, p2, p3, p4;
-	private Vector v1, v2, v3, v4;
+	private Line2D v1, v2, v3, v4;
 
 	private ArrayList<Vertex> vertices;
-	private ArrayList<Vector> vectors;
+	private ArrayList<Line2D> vectors;
 
 	public Quad(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
 
@@ -22,13 +24,17 @@ public class Quad {
 		p3 = new Vertex(x3, y3);
 		p4 = new Vertex(x4, y4);
 
-		v1 = new Vector(p1, p2);
-		v2 = new Vector(p2, p3);
-		v3 = new Vector(p3, p4);
-		v4 = new Vector(p4, p1);
+		v1 = new Line2D();
+		v1.setLine(p1.getX(), p1.getY(), p2.getX(), p2.getY());
+		v2 = new Line2D();
+		v2.setLine(p2.getX(), p2.getY(), p3.getX(), p3.getY());
+		v3 = new Line2D();
+		v3.setLine(p3.getX(), p3.getY(), p4.getX(), p4.getY());
+		v4 = new Line2D();
+		v4.setLine(p4.getX(), p4.getY(), p1.getX(), p1.getY());
 
 		vertices = new ArrayList<Vertex>(Arrays.asList(p1, p2, p3, p4));
-		vectors = new ArrayList<Vector>(Arrays.asList(v1, v2, v3, v4));
+		vectors = new ArrayList<Line2D>(Arrays.asList(v1, v2, v3, v4));
 
 	}
 
@@ -36,7 +42,7 @@ public class Quad {
 		return vertices;
 	}
 
-	public ArrayList<Vector> getVectors() {
+	public ArrayList<Line2D> getVectors() {
 		return vectors;
 	}
 
